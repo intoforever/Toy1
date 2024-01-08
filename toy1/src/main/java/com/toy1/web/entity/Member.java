5b657ee8c8d58da2e5b3427e0718bbef2519bfb3
sf33 <startingfrom33@gmail.com>
2024-01-08 17:23:35 +0900

    Member 클래스에 GeneratedValue 추가하여 시퀀스 설정

--- toy1/src/main/java/com/toy1/web/entity/Member.java
+++ toy1/src/main/java/com/toy1/web/entity/Member.java
@@ -1,6 +1,8 @@
 package com.toy1.web.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -15,6 +17,7 @@ import lombok.NoArgsConstructor;
 public class Member {
 	
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String username;
 	private String password;
