f2a78e7ff111f025946310a7c302c78b58cc9187
sf33 <startingfrom33@gmail.com>
2024-01-08 17:24:03 +0900

    PrimayKeys 복합키 클래스를 set할 수 있게 어노테이션 추가

--- toy1/src/main/java/com/toy1/web/entity/PrimaryKeys.java
+++ toy1/src/main/java/com/toy1/web/entity/PrimaryKeys.java
@@ -3,9 +3,15 @@ package com.toy1.web.entity;
 import java.io.Serializable;
 
 import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
+@Builder
 @Embeddable
 public class PrimaryKeys implements Serializable {
 	
