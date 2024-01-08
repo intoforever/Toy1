1e2e6c86bdfc9694b683b57723f8a5076f706b86
sf33 <startingfrom33@gmail.com>
2024-01-08 17:22:47 +0900

    MemberRoleRepository와 MemberRoleViewRepository 분리

--- toy1/src/main/java/com/toy1/web/repository/MemberRoleRepository.java
+++ toy1/src/main/java/com/toy1/web/repository/MemberRoleRepository.java
@@ -1,24 +1,12 @@
 package com.toy1.web.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.toy1.web.entity.MemberRoleView;
+import com.toy1.web.entity.MemberRole;
 import com.toy1.web.entity.PrimaryKeys;
 
 @Repository
-public interface MemberRoleRepository extends JpaRepository<MemberRoleView, PrimaryKeys> {
+public interface MemberRoleRepository extends JpaRepository<MemberRole, PrimaryKeys>{
 
-	List<MemberRoleView> findAllByPrimaryKeyMemberId(Long memberId);
-	
-//	@Query(value = "SELECT " +
-//            "mr.member_id, " +
-//            "mr.role_id, " +
-//            "r.name AS role_name " +
-//            "FROM member_role mr " +
-//            "LEFT JOIN role r ON mr.role_id = r.id", 
-//    nativeQuery = true)
-//	List<MemberRoleView> findAllByMemberId(Long id);
-}
+}//class ends
---
+++ toy1/src/main/java/com/toy1/web/repository/MemberRoleViewRepository.java
@@ -0,0 +1,16 @@
+package com.toy1.web.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.toy1.web.entity.MemberRoleView;
+import com.toy1.web.entity.PrimaryKeys;
+
+@Repository
+public interface MemberRoleViewRepository extends JpaRepository<MemberRoleView, PrimaryKeys> {
+
+	List<MemberRoleView> findAllByPrimaryKeyMemberId(Long memberId);
+
+}//class ends
